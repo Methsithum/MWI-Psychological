d68866a4fd05914826c1890b98feebc6f1fdf382
DamikaDeshan <damikadeshan40@gmail.com>
2026-05-19 16:48:07 +0530

    Update courseController.js

--- backend/controllers/courseController.js
+++ backend/controllers/courseController.js
@@ -2,12 +2,20 @@ const asyncHandler = require('../utils/asyncHandler');
 const ApiError = require('../utils/ApiError');
 const Course = require('../models/Course');
 
+const MAX_COURSES = 2;
+
 const getCourses = asyncHandler(async (req, res) => {
   const courses = await Course.find().populate('teacher', 'firstName lastName email').populate('students', 'firstName lastName email');
   res.status(200).json({ success: true, count: courses.length, data: courses });
 });
 
 const createCourse = asyncHandler(async (req, res) => {
+  const courseCount = await Course.countDocuments();
+
+  if (courseCount >= MAX_COURSES) {
+    throw new ApiError(400, 'Course limit reached. This project allows only two courses.');
+  }
+
   const course = await Course.create({ ...req.body, teacher: req.user._id });
   res.status(201).json({ success: true, message: 'Course created', data: course });
 });
